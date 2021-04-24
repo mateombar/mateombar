c7d05eb75a213fd57d4337c7cae43a5b8a6a84d1
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-04-24 08:03:50 -0500

    Navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/Navbar.scss';
 import logo from '../assets/svg/logo__white.svg';
 import menu from '../assets/svg/hamurger.svg';
 export const Navbar = () => {
     const [isMenu, setIsMenu] = useState(false);
+    const [navbarLinks, setNavbarLinks] = useState("navbar__links");
+
+    useEffect(() => {
+        if (isMenu) {
+            setNavbarLinks("navbar__links navbar__links--active");
+        } else if (navbarLinks === 'navbar__links') {
+
+        } else {
+            setTimeout(() => {
+                setNavbarLinks("navbar__links")
+            }, 400);
+            setNavbarLinks("navbar__links navbar__links--deactive")
+        }
+    }, [isMenu])
 
     return (
-        <nav className="navbar">
-            <div className={isMenu ? 'navbar__container navbar__container--active' : 'navbar__container'}>
+        <nav className={isMenu ? "navbar navbar--menu" : "navbar"}>
+            <div className="navbar__container">
                 <div className="navbar__logo">
                     <img src={logo} alt="Logo" />
                     <span>Mateo</span>
                 </div>
-                <ul className="navbar__links">
+                <ul className={navbarLinks}>
                     <li onClick={() => setIsMenu(false)} className="link"><a href="#about">About</a></li>
                     <li onClick={() => setIsMenu(false)} className="link"><a href="#projects">Projects</a></li>
                     <li onClick={() => setIsMenu(false)} className="link"><a href="#contact">Contact</a></li>
