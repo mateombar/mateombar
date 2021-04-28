5ea662532b4241be2d4c8c715092d0027faab55c
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-04-28 18:02:27 -0500

    [add] Animations.scss

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useNearScreen } from '../hooks/useNearScreen';
 import './styles/Project.scss';
 export const Project = ({ project }) => {
+    const [show, refElement] = useNearScreen();
     return (
-        <article className="project">
-            <a href={project.pageUrl} target="_blank">
-                <img className="project__image" src={project.image} alt={project.title} />
-            </a>
-            <div className="project__description">
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
-            </div>
+        <article className="project" ref={refElement}>
+            {show &&
+                <div className="project__container">
+                    <a href={project.pageUrl} target="_blank">
+                        <img className="project__image" src={project.image} alt={project.title} />
+                    </a>
+                    <div className="project__description">
+                        <h2>{project.title}</h2>
+                        <p>{project.description}</p>
+                    </div>
+                </div>
+            }
         </article>
     )
 }
